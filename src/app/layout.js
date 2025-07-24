7c42b592d958b65edc271c509ddc21cfa0c79c91
farzalfian <ff.fian29@gmail.com>
2025-07-24 18:19:06 +0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }) {
   return (
@@ -8,6 +9,7 @@ export default function RootLayout({ children }) {
       <body className="bg-black text-white">
         <Navbar/>
         {children}
+        <Analytics/>
         <Footer/>
       </body>
     </html>
